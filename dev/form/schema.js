8c587cac4e2f5a404b0217f97dc303150fd36500
David Higgins <dhiggins@aimmedia.com>
2018-04-22 11:59:21 -0400

    added "form" to dev

---
+++ dev/form/schema.js
@@ -0,0 +1,92 @@
+import fecha from "fecha";
+
+import {
+    validators
+} from "../../src";
+
+let customAsyncValidator = function(value) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (value)
+                resolve();
+            else
+                resolve([ "Invalid value from async validator" ]);
+        }, 1000);
+    });
+};
+
+module.exports = {
+    fields: [
+
+        /***********/
+        /*  INPUT  */
+        /***********/
+        {
+            type: "input",
+            inputType: "hidden",
+            label: "--- INPUT ---",
+            model: "",
+            styleClasses: "alert alert-info"
+        }, {
+            type: "input",
+            inputType: "hidden",
+            label: "Hidden",
+            model: "id",
+            inputName: "hiddenField"
+        }, {
+            type: "input",
+            inputType: "text",
+            label: "First name",
+            model: "firstName",
+            featured: true,
+            required: true,
+            help: "First name of user",
+            placeholder: "User's first name",
+            styleClasses: "half-width",
+            validator: validators.string,
+            onChanged(model, newVal, oldVal, field) {
+                console.log(`Model's name changed from ${oldVal} to ${newVal}. Model:`, model);
+            },
+            onValidated(model, errors, field) {
+                if (errors.length > 0)
+                    console.warn("Validation error in Name field! Errors:", errors);
+            }
+        }, {
+            type: "input",
+            inputType: "text",
+            label: "Last name",
+            model: "lastName",
+            featured: true,
+            required: true,
+            placeholder: "User's last name",
+            styleClasses: "half-width",
+            validator: validators.string
+        }, {
+            type: "input",
+            inputType: "url",
+            label: "URL",
+            model: "website",
+            placeholder: "Enter your website",
+            inputName: "website",
+            validator: customAsyncValidator //validators.url
+        }, {
+            type: "input",
+            inputType: "tel",
+            label: "Telephone",
+            model: "phone",
+            placeholder: "Enter your phone number",
+            styleClasses: "half-width"
+        }, {
+            type: 'submit',
+            validateBeforeSubmit: true,
+            onSubmit(model, schema, $event) {
+                // $event.preventDefault();
+                console.log('submit', model, schema, $event);
+            },
+            onValidationError(model, schema, errors, $event) {
+                // $event.preventDefault();
+                console.log('errors', model, schema, errors, $event);
+            }
+        }
+    ]
+};
